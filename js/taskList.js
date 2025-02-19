e79e6a25773b9652cedf9c4f23a84779c8866dea
MaddalenaRen <ileniarennella@gmail.com>
2025-02-19 16:20:47 +0100

    commit-1

---
+++ js/taskList.js
@@ -0,0 +1,29 @@
+const pageForm = document.getElementById('task-list-form')
+const listContainer = document.getElementById('saved-elements')
+
+const nameInput = document.getElementById('name')
+pageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const elemento = {
+        name: nameInput.value
+    }
+    console.log('elemento', elemento)
+
+    const cardList = document.createElement('li')
+    cardList.innerText = elemento.name
+
+    cardList.addEventListener('click', () => {
+        cardList.classList.toggle('completed')
+    })
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'Elimina'
+    deleteButton.addEventListener('click', (e) => { cardList.remove() })
+
+    cardList.appendChild(deleteButton)
+    listContainer.appendChild(cardList)
+
+
+
+})
+
